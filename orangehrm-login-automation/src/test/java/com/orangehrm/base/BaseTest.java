0ade2d24aa36a09e581e0302ddd4efbe003c6af1
S-nusrath <nusrath4786@gmail.com>
2026-01-05 20:33:01 +0530

    Added try catch for the test cases

--- orangehrm-login-automation/src/test/java/com/orangehrm/base/BaseTest.java
+++ orangehrm-login-automation/src/test/java/com/orangehrm/base/BaseTest.java
@@ -1,79 +1,17 @@
-// package com.orangehrm.base;
-
-// import java.time.Duration;
-
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.openqa.selenium.chrome.ChromeOptions;
-// import org.openqa.selenium.PageLoadStrategy;
-// import org.testng.ITestResult;
-// import org.testng.annotations.AfterMethod;
-// import org.testng.annotations.BeforeMethod;
-
-// import com.orangehrm.utils.ScreenshotUtil;
-
-// public class BaseTest {
-
-//     protected WebDriver driver;
-
-//     //@BeforeMethod
-//     // public void setUp() {
-//     //     ChromeOptions options = new ChromeOptions();
-//     //     options.setPageLoadStrategy(PageLoadStrategy.EAGER);
-
-//     //     driver = new ChromeDriver(options);
-//     //     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//     //     driver.manage().window().maximize();
-//     //     driver.get("https://opensource-demo.orangehrmlive.com/");
-//     // }
-//     @BeforeMethod
-// public void setUp() {
-
-//     ChromeOptions options = new ChromeOptions();
-//     options.addArguments("--remote-allow-origins=*");
-//     options.addArguments("--disable-notifications");
-//     options.addArguments("--disable-infobars");
-//     options.addArguments("--disable-extensions");
-//     options.addArguments("--start-maximized");
-
-//     driver = new ChromeDriver(options);
-
-//     driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(120));
-//     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
-//     driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-// }
-
-
-//     @AfterMethod
-//     public void tearDown(ITestResult result) {
-
-//         // Take screenshot ONLY if test fails
-//         if (ITestResult.FAILURE == result.getStatus()) {
-//             ScreenshotUtil.takeScreenshot(driver, result.getName());
-//         }
-
-//         if (driver != null) {
-//             driver.quit();
-//         }
-//     }
-
-//      public WebDriver getDriver() {
-//         return driver;
-//     }
-// }
-
 package com.orangehrm.base;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import com.orangehrm.utils.ScreenshotUtil;
 
 public class BaseTest {
 
@@ -90,13 +28,25 @@ public class BaseTest {
     @AfterMethod
     public void tearDown(ITestResult result) {
 
-        // 🔥 THIS IS THE KEY PART
         if (ITestResult.FAILURE == result.getStatus()) {
-            ScreenshotUtil.takeScreenshot(driver, result.getName());
+            takeScreenshot(result.getName());
         }
 
         if (driver != null) {
             driver.quit();
+        }
+    }
+
+   
+    public void takeScreenshot(String testName) {
+        try {
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(
+                src,
+                new File("screenshots/" + testName + ".png")
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
